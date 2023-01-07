6f5f1a0c2b6d461287497106dea1084099efe309
Jackson5551 <45270054+Jackson5551@users.noreply.github.com>
2023-01-07 10:29:31 -0700

    fixed search page width bug

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -4,7 +4,7 @@ const Loading = () => {
   document.title = "Loading..."
   return (
     <div
-    className='flex justify-center items-center bg-slate-800 text-white h-screen w-screen'>
+    className='flex justify-center items-center bg-slate-800 text-white h-screen w-full'>
         <h1 className='text-2xl'>Loading...</h1>
     </div>
   )
